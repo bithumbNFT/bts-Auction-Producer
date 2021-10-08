f8f3bb96e624b31623b0aaedb48976211d99271e
msj0319 <gaeddongie13@gmail.com>
2021-10-08 21:51:45 +0900

    producer log

--- src/main/java/bts/auction/api/producer/controller/AuctionController.java
+++ src/main/java/bts/auction/api/producer/controller/AuctionController.java
@@ -10,6 +10,7 @@ import bts.auction.api.producer.service.KafkaProducer;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Api(value = "Auction Producer Controller")
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auction")
@@ -28,14 +30,16 @@ public class AuctionController {
     private final AuctionService auctionService;
     private final TimeRepository timeRepository;
 
+    @ApiOperation(value = "경매 시작 시, 경매 마감 시간 설정")
     @PostMapping("/start")
     public String Auctionstart(@RequestBody TimerDto timeDto){
-        LocalTime localTime = LocalTime.now().plusMinutes(timeDto.getTime());
+        LocalTime localTime = LocalTime.now().plusMinutes(timeDto.getTime()); //현재 시간 + 설정 시간
         System.out.println(timeDto);
         Time endtime = Time.valueOf(localTime);
         Timer time = Timer.builder().NftId(timeDto.getNft())
                                   .time(endtime).build();
         timeRepository.save(time);
+        log.debug("========Auction Start========");
         return "timetable insert";
     }
 
@@ -52,6 +56,7 @@ public class AuctionController {
         //카프카 브로커로 메시지 전송
         kafkaProducer.sendMessage(produceAuction);
         //몽고 DB에 데이터 저장
+        log.debug("========Produce Message========");
         return "kafka produce";
     }
 }
